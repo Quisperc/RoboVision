6c8307e31b41384264d9b1578f7a6fe3edfd9674
Atest-1 <507678128@qq.com>
2025-02-09 21:10:56 +0800

    test

--- MainForm.cs
+++ MainForm.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
-using OpenCvSharp;
+using OpenCvSharp;// 没用上
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
